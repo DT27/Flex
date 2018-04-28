a46883f19cc1ad095253d1cc213f618230288674
DT27 <dragonet1943@gmail.com>
2018-04-28 19:46:08 +0800

    编译后

---
+++ build/config.js
@@ -0,0 +1,10 @@
+/**
+ * Created by DT27 on 2018/3/26.
+ */
+
+var domoticz = {
+    server: "/",
+    use_favorites: true,
+    colNum: 12,
+    rowHeight: 34,
+}
--- public/config.js
+++ public/config.js
@@ -1,7 +1,3 @@
-/**
- * Created by DT27 on 2018/3/26.
- */
-
 var domoticz = {
     server: "/",
     use_favorites: true,
